989749dc5219691df05c868055bec917c6ae927d
[umaidila] <[frenirus125@gmail.com]>
2021-10-05 19:48:09 +1000

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 print('хай мир')
+=======
+import cv2
+img = cv2.imread('girl.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('girl', img)
+cv2.waitKey(0)
+cv2.imwrite('graygirl.jpg', img)
+>>>>>>> 16a0cff (Initial commit)
