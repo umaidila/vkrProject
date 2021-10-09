2995fda4aefe31e737335337e86e5c46ae3e42b6
[umaidila] <[frenirus125@gmail.com]>
2021-10-10 00:14:12 +1000

    test1

--- main.py
+++ main.py
@@ -1,105 +1,106 @@
 from math import sqrt
-
+from time import sleep
+from tkinter import *
 import cv2
 import numpy as np
-print("enter monitor diagonal (inches): ")
-scr_diag = 21                  #int(input())  # monitor diagonal
-print("enter monitor height (pixels): ")
-scr_height = 768                       #int(input())
-print("enter monitor width (pixels): ")
-scr_width = 1024                       #int(input())
-
-black_cell = cv2.imread('black_cell.jpg')
-#black_cell = cv2.resize(black_cell,(500,500))
-
-white_cell = cv2.imread('white_cell.jpg')
-#white_cell = cv2.resize(white_cell,(250,250))
 
 
-window = cv2.imread('girl.jpg')
-window = cv2.resize(window,(scr_width,scr_height)) # как фон
+def showVideo():
 
-diag_pix = sqrt(scr_width**2 + scr_height**2) # диагональ в пикселях
-side = int(diag_pix/scr_diag)  # сторона клетки в пикселях (по идее 1 дюйм)
+    scr_diag = int(txt1.get())
+    scr_width = int(txt2.get())
+    scr_height = int(txt3.get())
 
-isBlackStart = False # тоже переключатель, задающий цвет в начале каждой строки
-x = 0
-y = 0 # бегунки заполняющие поле
-xArray = np.arange(0,scr_width,side)
-yArray = np.arange(0,scr_height,side) # координаты шахматной сетки по x и y
+    black_cell = cv2.imread('black_cell.jpg')
+    white_cell = cv2.imread('white_cell.jpg')
 
-#xArray = np.linspace(0,scr_widght,len(xArray)).round() # изменяем массив, чтобы не было обрезанных клеток
-#yArray = np.linspace(0,scr_widght,len(yArray)).round() # изменяем массив, чтобы не было обрезанных клеток
+    window = cv2.imread('girl.jpg')
+    window = cv2.resize(window, (scr_width, scr_height))  # как фон
 
+    diag_pix = sqrt(scr_width ** 2 + scr_height ** 2)  # диагональ в пикселях
+    side = int(diag_pix / scr_diag)  # сторона клетки в пикселях (по идее 1 дюйм)
 
-black_cell = cv2.resize(black_cell,(side,side))
-white_cell = cv2.resize(white_cell,(side,side))
+    isBlackStart = False  # тоже переключатель, задающий цвет в начале каждой строки
+    x = 0
+    y = 0  # бегунки заполняющие поле
+    xArray = np.arange(0, scr_width, side)
+    yArray = np.arange(0, scr_height, side)  # координаты шахматной сетки по x и y
 
-'''
-for i in range(len(yArray)-1):
-    isBlackStart = not isBlackStart
-    isBlack = isBlackStart
-    for j in range(len(xArray)-1):
-        if isBlack:
-            window[yArray[i]:yArray[i+1],xArray[j]:xArray[j+1]] = black_cell
-            isBlack = False
-        else:
-            window[yArray[i]:yArray[i+1], xArray[j]:xArray[j + 1]] = white_cell
-            isBlack = True
-'''
+    black_cell = cv2.resize(black_cell, (side, side))
+    white_cell = cv2.resize(white_cell, (side, side))
 
-for i in range(len(yArray)-1):
-    isBlackStart = not isBlackStart
-    isBlack = isBlackStart
-    for j in range(len(xArray)):
-        if j == len(xArray)-1: # если последняя клетка, то она будет обрезанной, нужно сначала изменить размер
+    for i in range(len(yArray) - 1):
+        isBlackStart = not isBlackStart
+        isBlack = isBlackStart
+        for j in range(len(xArray)):
+            if j == len(xArray) - 1:  # если последняя клетка, то она будет обрезанной, нужно сначала изменить размер
 
-            if isBlack:
-                black_cell = cv2.resize(black_cell,(scr_width-xArray[j],side))
-                window[yArray[i]:yArray[i+1],xArray[j]:scr_width] = black_cell
-                black_cell = cv2.resize(black_cell, (side, side)) # и обратно
+                if isBlack:
+                    black_cell = cv2.resize(black_cell, (scr_width - xArray[j], side))
+                    window[yArray[i]:yArray[i + 1], xArray[j]:scr_width] = black_cell
+                    black_cell = cv2.resize(black_cell, (side, side))  # и обратно
+                else:
+                    white_cell = cv2.resize(white_cell, (scr_width - xArray[j], side))
+                    window[yArray[i]:yArray[i + 1], xArray[j]:scr_width] = white_cell
+                    white_cell = cv2.resize(white_cell, (side, side))
             else:
-                white_cell = cv2.resize(white_cell, (scr_width - xArray[j] ,side))
-                window[yArray[i]:yArray[i + 1], xArray[j]:scr_width] = white_cell
-                white_cell = cv2.resize(white_cell, (side, side))
+                if isBlack:
+                    window[yArray[i]:yArray[i + 1], xArray[j]:xArray[j + 1]] = black_cell
+                    isBlack = False
+                else:
+                    window[yArray[i]:yArray[i + 1], xArray[j]:xArray[j + 1]] = white_cell
+                    isBlack = True
+
+    black_cell = cv2.resize(black_cell, (side, scr_height - yArray[len(yArray) - 1]))
+    white_cell = cv2.resize(white_cell, (side, scr_height - yArray[len(yArray) - 1]))
+    for i in range(len(xArray) - 1):  # заполение нижнего столбца
+        isBlackStart = not isBlackStart
+        if isBlackStart:
+            window[yArray[len(yArray) - 1]:scr_height, xArray[i]:xArray[i + 1]] = black_cell
         else:
-            if isBlack:
-                window[yArray[i]:yArray[i+1],xArray[j]:xArray[j+1]] =  black_cell
-                isBlack = False
-            else:
-                window[yArray[i]:yArray[i+1], xArray[j]:xArray[j + 1]] = white_cell
-                isBlack = True
+            window[yArray[len(yArray) - 1]:scr_height, xArray[i]:xArray[i + 1]] = white_cell
 
-
-black_cell = cv2.resize(black_cell,())
-for i in range(len(xArray)): # заполение нижнего столбца
     isBlackStart = not isBlackStart
-    if
+    # и последнюю клетку в углу:
+    if isBlackStart:
+        black_cell = cv2.resize(black_cell, (scr_width - xArray[len(xArray) - 1], scr_height - yArray[len(yArray) - 1]))
+        window[yArray[len(yArray) - 1]:scr_height, xArray[len(xArray) - 1]:scr_width] = black_cell
+    else:
+        white_cell = cv2.resize(white_cell, (scr_width - xArray[len(xArray) - 1], scr_height - yArray[len(yArray) - 1]))
+        window[yArray[len(yArray) - 1]:scr_height, xArray[len(xArray) - 1]:scr_width] = white_cell
+
+    cv2.imshow('program', window)
+    cv2.waitKey(0)
 
 
 
+window = Tk()
+window.title("hello world")
+window.geometry('300x150')
 
+lbl1 = Label(window,text="Диагональ монитора (дюйм): ",padx=20,pady=5)
+lbl1.grid(column=0,row=0)
+lbl2 = Label(window,text="Ширина монитора (пикс): ",pady=5)
+lbl2.grid(column=0,row=1)
+lbl3 = Label(window, text = "Высота монитора (пикс): ",pady=5)
+lbl3.grid(column=0,row=2)
 
-'''
-for i in range(len(xArray)-2):
-    for j in range(len(yArray)-2):
-        if isBlack:
-            window[yArray[j]:yArray[j+1],xArray[i]:xArray[i+1]] = black_cell
-            isBlack = False
-        else:
-            window[yArray[j]:yArray[j+1],xArray[i]:xArray[i+1]] = white_cell
-            isBlack = True
-    isBlack = False
-'''
-cv2.imshow('program',window)
-cv2.waitKey(0)
+var1 = IntVar()
+var1.set(21) # диагональ по умолчанию
+var2 = IntVar()
+var2.set(1024)
+var3 = IntVar()
+var3.set(768)
 
+txt1 = Spinbox(window,from_=17,to=40,width=7,textvariable=var1)
+txt1.grid(column=1,row=0)
 
+txt2 = Entry(window,width=8,textvariable=var2)
+txt2.grid(column=1,row=1)
+txt3 = Entry(window,width=8,textvariable=var3)
+txt3.grid(column=1,row=2)
 
- #   if x > scr_widght or y > scr_height:
-    #    flag = False
+startButton = Button(window,text="Начать",command=showVideo,pady=2)
+startButton.grid(column=0,row=3)
+window.mainloop()
 
-
-#black_cell[250:,250:] = white_cell
-#cv2.imshow('program',black_cell)
-##cv2.waitKey(0)
